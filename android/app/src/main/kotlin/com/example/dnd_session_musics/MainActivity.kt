a6d0f437611c4540ff8fa5d0194660b88792163f
halitmuhammed <skfstudio9@gmail.com>
2026-02-25 13:32:05 +0300

    init commit

---
+++ android/app/src/main/kotlin/com/example/dnd_session_musics/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dnd_session_musics
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:dnd_session_musics/app/app.bottomsheets.dart';
+import 'package:dnd_session_musics/app/app.dialogs.dart';
+import 'package:dnd_session_musics/app/app.locator.dart';
+import 'package:dnd_session_musics/app/app.router.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupLocator();
+  setupDialogUi();
+  setupBottomSheetUi();
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: Routes.startupView,
+      onGenerateRoute: StackedRouter().onGenerateRoute,
+      navigatorKey: StackedService.navigatorKey,
+      navigatorObservers: [StackedService.routeObserver],
+    );
+  }
+}
